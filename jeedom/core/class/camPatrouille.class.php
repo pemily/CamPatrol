fa4b1f955531caf046a33ecd7acec9b70b4e1824
pemily <pascal.emily@gmail.com>
2022-08-06 18:25:30 +0200

    upd

---
+++ jeedom/core/class/camPatrouille.class.php
@@ -0,0 +1,225 @@
+<?php
+/* This file is part of Jeedom.
+*
+* Jeedom is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Jeedom is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* * ***************************Includes********************************* */
+require_once __DIR__  . '/../../../../core/php/core.inc.php';
+
+class camPatrouille extends eqLogic {
+  /*     * *************************Attributs****************************** */
+
+  /*
+  * Permet de définir les possibilités de personnalisation du widget (en cas d'utilisation de la fonction 'toHtml' par exemple)
+  * Tableau multidimensionnel - exemple: array('custom' => true, 'custom::layout' => false)
+  public static $_widgetPossibility = array();
+  */
+
+  /*
+  * Permet de crypter/décrypter automatiquement des champs de configuration du plugin
+  * Exemple : "param1" & "param2" seront cryptés mais pas "param3"
+  public static $_encryptConfigKey = array('param1', 'param2');
+  */
+
+  /*     * ***********************Methode static*************************** */
+
+
+  // https://doc.jeedom.com/fr_FR/dev/daemon_plugin#Installation%20des%20d%C3%A9pendances
+  public static function dependancy_install() {
+    log::remove(__CLASS__ . '_update');    
+    return array(
+      'script' => dirname(__FILE__) . '/../../resources/install_apt.sh ' . jeedom::getTmpFolder(__CLASS__) . '/dependency_progress',
+      'log' => log::getPathToLog(__CLASS__ . '_update')
+    );
+  }
+  
+  
+	public static function dependancy_info() {
+    log::add(__CLASS__, 'info', 'dependancy_info');    
+		$return = array();
+		$return['log'] = log::getPathToLog(__CLASS__ . '_update');
+		$return['progress_file'] = jeedom::getTmpFolder(__CLASS__) . '/dependency_progress';
+		$return['state'] = 'ok';
+		if (exec('which npm | wc -l') == 0) {
+			$return['state'] = 'nok';
+		}
+		if (exec('which node | wc -l') == 0 && exec('which nodejs | wc -l') == 0) {
+			$return['state'] = 'nok';
+		}
+		return $return;
+	}
+
+  public static function deamon_info() {
+    // log::add(__CLASS__, 'info', 'deamon_info'); // méthode appellé toutes les 5 secondes par jeedom
+    $return = array();
+    $return['log'] = __CLASS__;
+    $return['state'] = 'nok';
+    $pid_file = jeedom::getTmpFolder(__CLASS__) . '/camPatrouille.pid';
+    if (file_exists($pid_file)) {
+        if (@posix_getsid(trim(file_get_contents($pid_file)))) {
+            $return['state'] = 'ok';
+        } else {
+            shell_exec(system::getCmdSudo() . 'rm -rf ' . $pid_file . ' 2>&1 > /dev/null');
+        }
+    }
+    $return['launchable'] = 'ok';
+    $user = config::byKey('username', __CLASS__); 
+    $pswd = config::byKey('password', __CLASS__); 
+    $port = config::byKey('port', __CLASS__); 
+    if ($user == '') {
+        $return['launchable'] = 'nok';
+        $return['launchable_message'] = __('Le nom d\'utilisateur n\'est pas configuré', __FILE__);
+    } elseif ($pswd == '') {
+        $return['launchable'] = 'nok';
+        $return['launchable_message'] = __('Le mot de passe n\'est pas configuré', __FILE__);
+    } elseif ($port == '') {
+        $return['launchable'] = 'nok';
+        $return['launchable_message'] = __('Le port n\'est pas configuré', __FILE__);
+    }    
+    return $return;
+}
+
+  /*
+  * Fonction exécutée automatiquement toutes les minutes par Jeedom
+  public static function cron() {}
+  */
+
+  /*
+  * Fonction exécutée automatiquement toutes les 5 minutes par Jeedom
+  public static function cron5() {}
+  */
+
+  /*
+  * Fonction exécutée automatiquement toutes les 10 minutes par Jeedom
+  public static function cron10() {}
+  */
+
+  /*
+  * Fonction exécutée automatiquement toutes les 15 minutes par Jeedom
+  public static function cron15() {}
+  */
+
+  /*
+  * Fonction exécutée automatiquement toutes les 30 minutes par Jeedom
+  public static function cron30() {}
+  */
+
+  /*
+  * Fonction exécutée automatiquement toutes les heures par Jeedom
+  public static function cronHourly() {}
+  */
+
+  /*
+  * Fonction exécutée automatiquement tous les jours par Jeedom
+  public static function cronDaily() {}
+  */
+
+  /*     * *********************Méthodes d'instance************************* */
+
+  // Fonction exécutée automatiquement avant la création de l'équipement
+  public function preInsert() {
+  }
+
+  // Fonction exécutée automatiquement après la création de l'équipement
+  public function postInsert() {
+  }
+
+  // Fonction exécutée automatiquement avant la mise à jour de l'équipement
+  public function preUpdate() {
+  }
+
+  // Fonction exécutée automatiquement après la mise à jour de l'équipement
+  public function postUpdate() {
+  }
+
+  // Fonction exécutée automatiquement avant la sauvegarde (création ou mise à jour) de l'équipement
+  public function preSave() {
+  }
+
+  // Fonction exécutée automatiquement après la sauvegarde (création ou mise à jour) de l'équipement
+  public function postSave() {
+  }
+
+  // Fonction exécutée automatiquement avant la suppression de l'équipement
+  public function preRemove() {
+  }
+
+  // Fonction exécutée automatiquement après la suppression de l'équipement
+  public function postRemove() {
+  }
+
+  /*
+  * Permet de crypter/décrypter automatiquement des champs de configuration des équipements
+  * Exemple avec le champ "Mot de passe" (password)
+  public function decrypt() {
+    $this->setConfiguration('password', utils::decrypt($this->getConfiguration('password')));
+  }
+  public function encrypt() {
+    $this->setConfiguration('password', utils::encrypt($this->getConfiguration('password')));
+  }
+  */
+
+  /*
+  * Permet de modifier l'affichage du widget (également utilisable par les commandes)
+  public function toHtml($_version = 'dashboard') {}
+  */
+
+  /*
+  * Permet de déclencher une action avant modification d'une variable de configuration du plugin
+  * Exemple avec la variable "param3"
+  public static function preConfig_param3( $value ) {
+    // do some checks or modify on $value
+    return $value;
+  }
+  */
+
+  /*
+  * Permet de déclencher une action après modification d'une variable de configuration du plugin
+  * Exemple avec la variable "param3"
+  public static function postConfig_param3($value) {
+    // no return value
+  }
+  */
+
+  /*     * **********************Getteur Setteur*************************** */
+
+}
+
+class templateCmd extends cmd {
+  /*     * *************************Attributs****************************** */
+
+  /*
+  public static $_widgetPossibility = array();
+  */
+
+  /*     * ***********************Methode static*************************** */
+
+
+  /*     * *********************Methode d'instance************************* */
+
+  /*
+  * Permet d'empêcher la suppression des commandes même si elles ne sont pas dans la nouvelle configuration de l'équipement envoyé en JS
+  public function dontRemoveCmd() {
+    return true;
+  }
+  */
+
+  // Exécution d'une commande
+  public function execute($_options = array()) {
+  }
+
+  /*     * **********************Getteur Setteur*************************** */
+
+}
--- jeedom/desktop/php/camPatrouille.php
+++ jeedom/desktop/php/camPatrouille.php
@@ -25,10 +25,10 @@ $eqLogics = eqLogic::byType($plugin->getId());
 				<span>{{Configuration}}</span>
 			</div>
 		</div>
-		<legend><i class="fas fa-table"></i> {{Mes templates}}</legend>
+		<legend><i class="fas fa-table"></i> {{Mes équipements}}</legend>
 		<?php
 		if (count($eqLogics) == 0) {
-			echo '<br><div class="text-center" style="font-size:1.2em;font-weight:bold;">{{Aucun équipement Template trouvé, cliquer sur "Ajouter" pour commencer}}</div>';
+			echo '<br><div class="text-center" style="font-size:1.2em;font-weight:bold;">{{Aucun équipement CamPatrouille trouvé, cliquer sur "Ajouter" pour commencer}}</div>';
 		} else {
 			// Champ de recherche
 			echo '<div class="input-group" style="margin:5px;">';
@@ -203,6 +203,6 @@ $eqLogics = eqLogic::byType($plugin->getId());
 </div><!-- /.row row-overflow -->
 
 <!-- Inclusion du fichier javascript du plugin (dossier, nom_du_fichier, extension_du_fichier, id_du_plugin) -->
-<?php include_file('desktop', 'template', 'js', 'template');?>
+<?php include_file('desktop', 'camPatrouille', 'js', 'camPatrouille');?>
 <!-- Inclusion du fichier javascript du core - NE PAS MODIFIER NI SUPPRIMER -->
 <?php include_file('core', 'plugin.template', 'js');?>
